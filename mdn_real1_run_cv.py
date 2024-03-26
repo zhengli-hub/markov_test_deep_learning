3afb0b0addacbbd34ce055c177b332ec44973437
Haoming Meng <hmeng29@wisc.edu>
2024-03-26 16:03:17 -0500

    revert changes

--- mdn_real1_run_cv.py
+++ mdn_real1_run_cv.py
@@ -87,9 +87,8 @@ config = Setting()
 
 pvalue_ls = []
 config.test_lag = int(config.dim + 1)
-tf.config.set_soft_device_placement(True)
-tf.config.threading.set_intra_op_parallelism_threads(19)
-tf.config.threading.set_inter_op_parallelism_threads(19)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+tf.debugging.set_log_device_placement(True)
 
 while True:
     try:
